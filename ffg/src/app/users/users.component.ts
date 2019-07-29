61d9b3eec5902201aaf1d1751df77a7baeefa661
thomastafel <tmtafel@gmail.com>
2019-07-29 14:28:32 -0400

    Update for new login

---
+++ ffg/src/app/users/users.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import * as admin from 'firebase-admin';
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    admin.initializeApp();
+  }
+
+}
